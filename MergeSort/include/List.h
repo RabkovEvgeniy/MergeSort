f7d207819ccd698f5b73485901fd64f632907f2d
Rabkov Evgeniy <rabkov_evgeniy@mail.ru>
2021-05-15 22:08:44 +0300

    Add template class list

--- MergeSort/include/List.h
+++ MergeSort/include/List.h
@@ -6,8 +6,35 @@ class List
 public:
 	List();
 
+
 	~List();
 
-private:
+protected:
+	
+	class Node {
+	public:
+		T data;
+		Node* next;
+		Node* prev;
 
+		Node(T Data);
+	};
+
+	Node* head;
+	Node* tail;
+	int size;
 };
+
+template<typename T>
+inline List<T>::Node::Node(T data)
+{
+	this->data = data;
+}
+
+template<typename T>
+inline List<T>::List()
+{
+	size = 0;
+	head = nullptr;
+	tail = nullptr;
+}
