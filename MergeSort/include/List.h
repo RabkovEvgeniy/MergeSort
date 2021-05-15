ce8d28a6d5e75137629b6ad12c240ed70799f3ea
Rabkov Evgeniy <rabkov_evgeniy@mail.ru>
2021-05-16 03:46:37 +0700

    Add push_front/back method in class List

--- MergeSort/include/List.h
+++ MergeSort/include/List.h
@@ -7,17 +7,20 @@ class List
 public:
 	List();
 
+	void push_front(T data);
+
+	void push_back(T data);
+
+
+protected:
 	class Node {
 	public:
 		Node(T Data);
 		T data;
-	private:
 		Node* next;
 		Node* prev;
 		friend List<T>;
 	};
-
-protected:
 	
 	Node* head;
 	Node* tail;
@@ -37,4 +40,44 @@ inline List<T>::List()
 	size = 0;
 	head = nullptr;
 	tail = nullptr;
+}
+
+template<typename T>
+inline void List<T>::push_front(T data)
+{
+	Node* new_node = new Node(data);
+	if (size != 0)
+	{
+		new_node->next = head;
+		new_node->prev = nullptr;
+		head = new_node;
+	}
+	else
+	{
+		head = new_node;
+		tail = new_node;
+		new_node->next = nullptr;
+		new_node->prev = nullptr;
+	}
+	size++;
+}
+
+template<typename T>
+inline void List<T>::push_back(T data)
+{
+	Node* new_node = new Node(data);
+	if (size != 0)
+	{
+		new_node->next = nullptr;
+		new_node->prev = tail;
+		tail = new_node;
+	}
+	else
+	{
+		head = new_node;
+		tail = new_node;
+		new_node->next = nullptr;
+		new_node->prev = nullptr;
+	}
+	size++;
 }
