66b854fb51241d7ab1d2813dc4cf7d2e9ef5c587
Rabkov Evgeniy <rabkov_evgeniy@mail.ru>
2021-05-16 18:52:40 +0700

    Add lab logic

--- MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort.cpp
@@ -2,7 +2,30 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+	int a_sum;
+	int b_sum;
+	int c_sum;
+	int run_c;
+	List<int> A,B,C;
+	for (int i = 0; i < 10; i++)
+	{
+		A.push_back(i);
+		B.push_back(i + 10);
+	}
 
+	a_sum = A.check_sum();
+	b_sum = B.check_sum();
+	
+	C.merge(A, B);
+	c_sum = C.check_sum();
+	run_c = C.run_num();
+
+	std::cout << "A sum = " <<a_sum << endl;
+	std::cout << "B sum = " <<b_sum << endl;
+	std::cout << "C sum = " <<c_sum << endl;
+	std::cout << "run in C = " << run_c << endl << endl;
 }
--- MergeSort/include/List.h
+++ MergeSort/include/List.h
@@ -12,10 +12,18 @@ public:
 	void push_back(T data);
 	
 	void pop_back();
+
+	void clear();
+
 	void pop_front();
+
+	T check_sum();
+
+	int run_num();
 
 	int get_size();
 	
+	void merge(List<T> &A, List<T> &B);
 
 	~List();
 
@@ -73,6 +81,7 @@ inline void List<T>::push_front(T data)
 	{
 		new_node->next = head;
 		new_node->prev = nullptr;
+		head->prev = new_node;
 		head = new_node;
 	}
 	else
@@ -91,8 +100,10 @@ inline void List<T>::push_back(T data)
 	Node* new_node = new Node(data);
 	if (size != 0)
 	{
+
 		new_node->next = nullptr;
 		new_node->prev = tail;
+		tail->next = new_node;
 		tail = new_node;
 	}
 	else
@@ -129,6 +140,13 @@ inline void List<T>::pop_back()
 }
 
 template<typename T>
+inline void List<T>::clear()
+{
+	while(size!=0)
+	pop_back();
+}
+
+template<typename T>
 inline void List<T>::pop_front()
 {
 	Node* ptr = head;
@@ -152,9 +170,57 @@ inline void List<T>::pop_front()
 }
 
 template<typename T>
+inline T List<T>::check_sum()
+{
+	T sum = 0;
+	for (Node* ptr = get_head(); ptr!=nullptr; ptr = ptr->next)
+	{
+		sum += ptr->data;
+	}
+	return sum;
+}
+
+template<typename T>
+inline int List<T>::run_num()
+{
+	int count=1;
+	for (Node* ptr = get_head(); ptr->next != nullptr; ptr = ptr->next)
+	{
+		if (ptr->data > ptr->next->data)
+			count++;
+	}
+	return count;
+}
+
+template<typename T>
 inline int List<T>::get_size()
 {
 	return this->size;
+}
+
+template<typename T>
+inline void List<T>::merge(List<T> &A, List<T> &B)
+{
+	this->clear();
+	while (A.size!=0 || B.size!=0 )
+	{
+		if(A.size == 0)
+		{
+			this->emplace_back_node(B.get_first_node());
+		}
+		else if (B.size == 0)
+		{
+			this->emplace_back_node(A.get_first_node());
+		}
+		else
+		{
+			if(A.head->data > B.head->data)
+				this->emplace_back_node(B.get_first_node());
+			else
+				this->emplace_back_node(A.get_first_node());
+
+		}
+	}
 }
 
 template<typename T>
@@ -198,6 +264,7 @@ inline void List<T>::emplace_back_node(Node* new_node)
 	{
 		new_node->next = nullptr;
 		new_node->prev = tail;
+		tail->next = new_node;
 		tail = new_node;
 	}
 	else
